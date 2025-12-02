9e9d692d7a23f008df8b0bb3b54ea816c4f22a4e
RebornEnder <enderop44rblx+bruh@gmail.com>
2025-12-02 20:54:36 +0100

    Refactor App.py to improve code structure

--- Data/App.py
+++ Data/App.py
@@ -210,8 +210,8 @@ if __name__ == '__main__':
     print(f'[+] Roblox GameJoin Status: {str(config["roblox"])}.')
     print(f'[+] Roblox Executable Path: {config["robloxpath"]}.')
     print(f'[+] Hosting Server On: http://0.0.0.0:8080.\n')
-    print("\n[!] Dedicated message from RebornEnder to 'JustAMale':\nplease dont steal stuff and claim it as your own :P this project took many weeks to complete and would like some credit!\n")
     server = WSGIServer(('0.0.0.0', 8080), app)
     server.serve_forever()
+
 
 
